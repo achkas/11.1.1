2bb11441d3418082e76773a9d1c5e8ec59d8d112
Igor Achkas <jigoren@yandex.ru>
2023-07-10 21:20:54 +0700

    add 1

---
+++ Client_Greeter.cpp
@@ -0,0 +1,23 @@
+﻿// Client_Greeter
+
+#include <iostream>
+#include <windows.h>
+#include <string>
+#include "Greeter.h"
+
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    Greeter::Greeter greeter;
+    std::string name;
+    
+    std::cout << "Введите имя:";
+    std::cin >> name;
+    greeter.set_inc(name);
+    std::cout << greeter.greet();
+
+}
